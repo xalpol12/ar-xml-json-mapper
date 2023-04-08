954753b0835a05fdf9bf0fd13c71fdfca307571a
Dawid Stawiszynski <dawid.stawiszynski@student.put.poznan.pl>
2023-04-08 19:05:03 +0200

    Fix indention bug

--- src/main/java/com/xalpol12/JAXBParser.java
+++ src/main/java/com/xalpol12/JAXBParser.java
@@ -5,20 +5,40 @@ import com.xalpol12.entity.Augmentation;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
+import javax.xml.transform.*;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.transform.stream.StreamSource;
 import java.io.File;
+import java.io.StringReader;
+import java.io.StringWriter;
 
 public class JAXBParser {
+
+    private static Transformer getConfiguredTransformer() throws TransformerConfigurationException {
+        Transformer transformer = TransformerFactory.newInstance().newTransformer();
+        transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+        transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "2");
+        return transformer;
+    }
+
+    private static Marshaller getConfiguredMarshaller() throws JAXBException {
+        JAXBContext jaxbContext = JAXBContext.newInstance(Augmentation.class);
+        Marshaller marshaller = jaxbContext.createMarshaller();
+        marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.FALSE);
+        return marshaller;
+    }
+
     public static <T> T unmarshall(String fileName, Class<T> type) throws JAXBException {
         File file = FileHandler.getFile(fileName);
         JAXBContext context = JAXBContext.newInstance(type);
         return (T) context.createUnmarshaller().unmarshal(file);
     }
 
-    public static void marshall(Augmentation augmentation, String newFileName) throws JAXBException {
-        JAXBContext jaxbContext = JAXBContext.newInstance(augmentation.getClass());
-        Marshaller marshaller = jaxbContext.createMarshaller();
-        marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
-        File file = new File(newFileName); // TODO: Check if filename ends with .xml
-        marshaller.marshal(augmentation, file);
+    public static void marshall(Augmentation augmentation, String newFileName) throws JAXBException, TransformerException {
+        Marshaller marshaller = getConfiguredMarshaller();
+        Transformer transformer = getConfiguredTransformer();
+        StringWriter stringWriter = new StringWriter();
+        marshaller.marshal(augmentation, stringWriter); // TODO: Check if filename ends with .xml
+        transformer.transform(new StreamSource(new StringReader(stringWriter.toString())), new StreamResult(new File(newFileName)));
     }
 }
--- src/main/java/com/xalpol12/Main.java
+++ src/main/java/com/xalpol12/Main.java
@@ -4,6 +4,7 @@ package com.xalpol12;
 import com.xalpol12.entity.*;
 
 import javax.xml.bind.JAXBException;
+import javax.xml.transform.TransformerException;
 
 public class Main {
 
@@ -11,7 +12,7 @@ public class Main {
     private static final String FILENAME = RESOURCES_PATH + "/documents/xml/Joining_01.xml";
     private static final String TEST_FILENAME = RESOURCES_PATH + "/documents/xml/Joining_01.xml";
 
-    public static void main(String[] args) throws JAXBException {
+    public static void main(String[] args) throws JAXBException, TransformerException {
         Augmentation augmentation = JAXBParser.unmarshall(TEST_FILENAME, Augmentation.class);
         System.out.println(augmentation.toString());
         JAXBParser.marshall(augmentation, "newFile.xml");
