ff4486f5741547f1001651dfdd28449a709636aa
Dawid Stawiszynski <dawid.stawiszynski@student.put.poznan.pl>
2023-05-19 15:05:44 +0200

    add new filehandler method, update dependencies

--- src/main/java/com/xalpol12/helper/FileHandler.java
+++ src/main/java/com/xalpol12/helper/FileHandler.java
@@ -1,9 +1,12 @@
 package com.xalpol12.helper;
 
 import java.io.*;
+import java.net.URISyntaxException;
+import java.net.URL;
 
 public class FileHandler {
-    public static File getFile(String fileName) {
-        return new File(fileName);
+    public static File getFile(String fileName) throws URISyntaxException {
+        URL url = FileHandler.class.getClassLoader().getResource(fileName);
+        return new File(url.toURI());
     }
 }
